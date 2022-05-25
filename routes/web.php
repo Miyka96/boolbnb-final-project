aad0f742ee3cce26895f770691b37489555a0a45
Simone Sada <si.sada93@gmail.com>
2022-05-25 17:14:14 +0200

    Install auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::fallback(function() {
    return view('home');
 });
