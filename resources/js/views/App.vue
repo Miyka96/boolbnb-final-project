106c389fe063a430ebfdd02562168a94cf650789
Simone Sada <si.sada93@gmail.com>
2022-05-25 17:08:13 +0200

    Setup Vue router

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <ul>
+         <li><router-link :to="{ name: 'ciao' }">Ciao</router-link></li>
+         <li><router-link :to="{ name: 'contact' }">Contact</router-link></li>
+      </ul>
+      <router-view></router-view>
+   </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
