ba86d9e26b16e488ddbff9cf13d282014be47311
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-05-26 12:25:09 +0200

    Position migrate and seed

--- database/migrations/2022_05_26_085501_create_positions_table.php
+++ database/migrations/2022_05_26_085501_create_positions_table.php
@@ -15,6 +15,12 @@ class CreatePositionsTable extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->float('latitude', 8, 6);
+            $table->float('longitude', 9, 6);
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
+            $table->string('zip_code');
             $table->timestamps();
         });
     }
--- database/seeds/PositionSeeder.php
+++ database/seeds/PositionSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Position;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PositionSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class PositionSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $position = new Position();
+            $position->latitude = $faker->latitude($min = -90, $max = 90);
+            $position->longitude = $faker->longitude($min = -180, $max = 180);
+            $position->address = $faker->streetAddress();
+            $position->city = $faker->city();
+            $position->country = $faker->country();
+            $position->zip_code = $faker->postcode();
+            $position->save();
+        }
     }
 }
