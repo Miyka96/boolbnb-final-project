ac9db732443496a7a29893c7cc867dd434956450
Miyka96 <micaelaseminara@outlook.it>
2022-05-30 00:19:37 +0200

    tentativo semi fallimentare, controllare domani mattina

--- app/Position.php
+++ app/Position.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Psr\Http\Message\RequestInterface;
 
 class Position extends Model
 {
@@ -10,7 +13,23 @@ class Position extends Model
         return $this->hasMany('App\House');
     }
 
-    public function geocoder(){
-        
+    public function geocoder(Request $request){
+        $address = $request->input('address');
+        $city = $request->input('city');
+        $country = $request->input('country');
+
+        // chiamata per geocoding tomtom ?! ps. il tutto è stato preso da postman, non giudicatemi
+            $client = new Client();
+            $request = new Request(["'GET', 'https://api.tomtom.com/search/2/geocode/.$address .$city .$country .json?key=DINngHSiTz58Z5fDF5pThkg1IrJA87je&limit=1'"]);
+            $res = $client->sendAsync($request)->wait();
+            echo $res->getBody();
+
+            $latitude = $res->position->lat;
+            $longitude= $res->position->lon; 
+
+
+            return compact('latitude', 'longitude');
+
+            // questa cosa è un prototipo, non so bene cosa ho fatto però boh è mezzanotte, pietà
     }
-}
+}     
