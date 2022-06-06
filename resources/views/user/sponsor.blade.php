1639f43cb493b85016ca23c7ba35e38dc14c67fe
Miyka96 <micaelaseminara@outlook.it>
2022-06-06 16:14:24 +0200

    up

--- resources/views/user/sponsor.blade.php
+++ resources/views/user/sponsor.blade.php
@@ -116,18 +116,65 @@ method="post">
       <div class="close">
           <i onclick="paymentHide()" class="fa-regular fa-circle-xmark"></i>
       </div>
-      <form class="input_wrapper" action="" method="POST">
-        @csrf
-          <input type="text" name="card_num" placeholder="inserisci il numero della carta">
-          <input type="text" name="card_cvc" placeholder="inserisci il cvc della carta">
-          <input type="text" name="card_date" placeholder="inserisci la data di scadenza della carta">
-          <div class="button_wrapper d-flex justify-content-center">
-              <button type="submit" id="button_carta" class="button">Start Sponsorship</button>
-          </div>
-      </form>
+      <script src="https://js.braintreegateway.com/web/dropin/1.33.2/js/dropin.js"></script>
+
+      <div id="dropin-container"></div>
+      <button id="submit-button" class="button button--small button--green">Purchase</button>
   </div>
 
 </div>
+<script>
+var button = document.querySelector('#submit-button');
+
+braintree.dropin.create({
+  authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+  selector: '#dropin-container'
+}, function (err, instance) {
+  button.addEventListener('click', function () {
+    instance.requestPaymentMethod(function (err, payload) {
+      // Submit payload.nonce to your server
+    });
+  })
+});
+</script>
 
 @endsection
+
+
+
+<style>
+  .button {
+  cursor: pointer;
+  font-weight: 500;
+  left: 3px;
+  line-height: inherit;
+  position: relative;
+  text-decoration: none;
+  text-align: center;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  display: inline-block;
+}
+
+.button--small {
+  padding: 10px 20px;
+  font-size: 0.875rem;
+}
+
+.button--green {
+  outline: none;
+  background-color: #64d18a;
+  border-color: #64d18a;
+  color: white;
+  transition: all 200ms ease;
+}
+
+.button--green:hover {
+  background-color: #8bdda8;
+  color: white;
+}
+</style>
 
