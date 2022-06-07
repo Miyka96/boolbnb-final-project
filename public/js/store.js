bd33faf7f2d725f340848b372b3018be65698f6a
marco.corallo.1997@gmail.com <marco.corallo.1997@gmail.com>
2022-06-07 15:03:22 +0200

    run watch

--- public/js/store.js
+++ public/js/store.js
@@ -12699,7 +12699,7 @@ function thumbAnimation() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Users\Simone\Documents\Progetti\boolbnb-final-project\resources\js\store.js */"./resources/js/store.js");
+module.exports = __webpack_require__(/*! /Users/marcocorallo/Desktop/boolbnb-final-project/resources/js/store.js */"./resources/js/store.js");
 
 
 /***/ })
