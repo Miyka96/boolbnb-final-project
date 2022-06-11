304752035b2a44cd0e8f0d10b0f2c69fb8f0ba50
marco.corallo.1997@gmail.com <marco.corallo.1997@gmail.com>
2022-06-11 16:27:53 +0200

    fixed team

--- public/js/admin.js
+++ public/js/admin.js
@@ -57887,8 +57887,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/asiademartino/Desktop/progetto_finale/boolbnb-final-project/resources/js/admin.js */"./resources/js/admin.js");
-module.exports = __webpack_require__(/*! /Users/asiademartino/Desktop/progetto_finale/boolbnb-final-project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/marcocorallo/Desktop/boolbnb-final-project/resources/js/admin.js */"./resources/js/admin.js");
+module.exports = __webpack_require__(/*! /Users/marcocorallo/Desktop/boolbnb-final-project/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
